ea47f6023ea8e266aa1507d567e62f5ba6442e33
VanessaBlacow-Berggren <44009018+VanessaBlacow-Berggren@users.noreply.github.com>
2019-10-14 00:25:09 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+let noiseScale=0.02;
+
 function setup() {
   createCanvas(400, 400);
+  background(0);
+  colorMode(HSB, 360, 100, 100);
 }
 
 function draw() {
-  background(220);
+
+  // console.log(frameCount % 360);
+
+  if(mouseIsPressed) {
+    background(0, 0.03);
+    stroke(frameCount % 360, 100, 100);
+    fill(frameCount % 360, 100, 100, 0.7);
+    beginShape();
+        vertex(mouseX, mouseY);
+        vertex(mouseX + width/20, mouseY + height/20);
+        vertex(pmouseX + width/20, pmouseY + height/20);
+        vertex(pmouseX, pmouseY);
+    endShape(CLOSE);
+
+    for (let x=0; x < width; x++) {
+    let noiseVal = noise((mouseX+x)*noiseScale, mouseY*noiseScale);
+    stroke(360 - frameCount % 360, 100, 100, 0.005);
+    line(x, mouseY + noiseVal * 80, x, 0);
+    }
+}
 }
