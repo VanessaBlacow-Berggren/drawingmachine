1d4844cccf7a6a277ed3c0f2f2ef7fcf18ca5c80
VanessaBlacow-Berggren <44009018+VanessaBlacow-Berggren@users.noreply.github.com>
2019-10-14 02:00:30 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
-let noiseScale=0.02;
+let array = [];
 
 function setup() {
   createCanvas(400, 400);
-  background(0);
+  // background(0);
+  drawGrid();
   colorMode(HSB, 360, 100, 100);
 }
 
@@ -20,11 +21,42 @@ function draw() {
         vertex(pmouseX + width/20, pmouseY + height/20);
         vertex(pmouseX, pmouseY);
     endShape(CLOSE);
-
-    for (let x=0; x < width; x++) {
-    let noiseVal = noise((mouseX+x)*noiseScale, mouseY*noiseScale);
-    stroke(360 - frameCount % 360, 100, 100, 0.005);
-    line(x, mouseY + noiseVal * 80, x, 0);
+    array.push([mouseX, mouseY]);
     }
+}
+
+function keyTyped() {
+
+console.log(`key ${key} is being typed`)
+  if (key === 's'){
+
+  }
+  else if (key === 'd'){
+    // background(0);
+    stroke(0);
+    fill(0);
+    // console.log(array);
+    for(let i = 0; i< array.length - 1; i++){
+    beginShape();
+      vertex(array[i][0], array [i][1]);
+      vertex(array[i][0] + width/20, array [i][1] + height/20);
+      vertex(array[i + 1][0] + width/20, array[i + 1][1] + height/20);
+      vertex(array[i + 1][0], array[i + 1][1]);
+      endShape(CLOSE);
+    }
+  }
+}
+
+function drawGrid() {
+  background(0);
+  stroke(360);
+  fill(0);
+
+  numCells = 20;
+
+  for (let j = 0; j <= width; j += width/ numCells){
+    for (let h = 0; h<= height; h += height/numCells){
+    rect(j, h, width/ numCells, height/ numCells );
+  }
 }
 }
